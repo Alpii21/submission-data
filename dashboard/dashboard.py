4b04cd86733b6b7ede0831f18ae7d026ce083c8d
Alpii21 <fanikanjeng@gmail.com>
2025-03-08 18:21:14 +0700

    ganti file

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataset
-orders_df = pd.read_csv("/mnt/data/orders_dataset.csv", parse_dates=["order_purchase_timestamp", "order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date"])
-customers_df = pd.read_csv("/mnt/data/customers_dataset.csv")
+orders_df = pd.read_csv("C:\dicoding\submission\data\orders_dataset.xls", parse_dates=["order_purchase_timestamp", "order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date"])
+customers_df = pd.read_csv("C:\dicoding\submission\data\customers_dataset.xls")
 
 # Sidebar - Filter
 st.sidebar.header("Filter Data")
